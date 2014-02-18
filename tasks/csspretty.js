e3eb7ca7c88c4346273cfddc9b9529d11bba99de
Hideki Abe <hideki.abe.0630@gmail.com>
2014-02-18 14:37:28 +0900

    Separeted processor creation logic

--- tasks/csspretty.js
+++ tasks/csspretty.js
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
 
-  var postcss = require('postcss');
+  var cssprettifier = require('css-prettifier');
 
   grunt.registerMultiTask('csspretty', 'CSS prettify your liking.', function() {
     // Merge task-specific and/or target-specific options with these defaults.
@@ -21,50 +21,9 @@ module.exports = function(grunt) {
       selectors: 'sameline'
     });
 
-    function _setOptions(node, nodeName, permitProp) {
-      for (var key in options[nodeName]) {
-        if (permitProp.indexOf(key) > -1 && options[nodeName][key] != null) {
-          node[key] = options[nodeName][key];
-        }
-      }
-    }
-
-    function makePrettifier() {
-      return postcss(function (css) {
-        css.eachDecl(function (decl) {
-          _setOptions(decl, 'decl', ['before', 'between']);
-
-          if (decl.parent.parent.type === 'atrule' && options.atRule.indent != null) {
-            decl.before = decl.before.replace(/(\r?\n)+\s*/, '$1') + 
-                          options.atRule.indent + options.atRule.indent;
-          }
-        });
-        css.eachRule(function (rule) {
-          var indent = '';
-
-          _setOptions(rule, 'rule', ['before', 'between', 'after']);
-
-          if (rule.parent.type === 'atrule' && options.atRule.indent != null) {
-            rule.before = rule.before.replace(/(\r?\n)+\s*/, '$1') + options.atRule.indent;
-            rule.after = rule.after.replace(/(\r?\n)+\s*/, '$1') + options.atRule.indent;
-            indent = options.atRule.indent;
-          }
-
-          if (options.selectors === 'separateline') {
-            rule.selector = rule.selector.replace(/, /g, ',\n' + indent);
-          } else {
-            rule.selector = rule.selector.replace(/,\n(\s+)?/g, ', ');
-          }
-        });
-        css.eachAtRule(function (atRule) {
-          _setOptions(atRule, 'atRule', ['before', 'between', 'after']);
-        });
-      });
-    }
-
     function csspretty(src, dest) {
-      var prettifier = makePrettifier();
-      var result = prettifier.process(src).css.trim();
+      var processor = cssprettifier(options);
+      var result = processor.process(src).css.trim();
       grunt.file.write(dest, result);
     }
 
