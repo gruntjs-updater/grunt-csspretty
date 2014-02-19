7807894a3d30817f5b4c3c5a2c04cdc92030d173
Hideki Abe <hideki.abe.0630@gmail.com>
2014-02-19 19:16:15 +0900

    SourceMaps support

--- tasks/csspretty.js
+++ tasks/csspretty.js
@@ -18,13 +18,22 @@ module.exports = function(grunt) {
   grunt.registerMultiTask('csspretty', 'CSS prettify your liking.', function() {
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
-      selectors: 'sameline'
+      selectors: 'sameline',
+      map: false
     });
 
-    function csspretty(src, dest) {
+    function csspretty(src, srcPath, destPath) {
       var processor = cssprettifier(options);
-      var result = processor.process(src).css.trim();
-      grunt.file.write(dest, result);
+      var result = processor.process(src, {
+        map: options.map,
+        from: srcPath,
+        to: destPath
+      });
+
+      grunt.file.write(destPath, result.css);
+      if (options.map) {
+        grunt.file.write(destPath + '.map', result.map);
+      }
     }
 
     // Iterate over all specified file groups.
@@ -43,7 +52,7 @@ module.exports = function(grunt) {
         var dest = f.dest || filepath;
         var src = grunt.file.read(filepath);
         
-        csspretty(src, dest);
+        csspretty(src, filepath, dest);
         
         // Print a success message.
         grunt.log.writeln('File "' + dest + '" created.');
